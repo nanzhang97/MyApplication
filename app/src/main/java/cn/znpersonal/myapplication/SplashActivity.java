28a11aae2879b133c14f79674d053b6c94001d3b
nanzhang <nanzhang1997@foxmail.com>
2022-02-24 03:02:37 +0800

    第一次上传

---
+++ app/src/main/java/cn/znpersonal/myapplication/SplashActivity.java
@@ -0,0 +1,32 @@
+package cn.znpersonal.myapplication;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+
+public class SplashActivity extends AppCompatActivity {
+    private VideoView mVideoView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        mVideoView = (VideoView) findViewById(R.id.videoview_splash);
+        mVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + File.separator + R.raw.splash));
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.start();
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+}
